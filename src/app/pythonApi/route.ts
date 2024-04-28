74d366be4b9ad75aabbb0a875b4c429808bd7c99
dkgkejdrb <70058394+dkgkejdrb@users.noreply.github.com>
2024-04-28 10:47:33 +0900

    테스트

--- src/app/pythonApi/route.ts
+++ src/app/pythonApi/route.ts
@@ -32,7 +32,7 @@ export async function POST(request: Request) {
         // 제출하기에서 사용하는 모듈
         temperature: 0.2, // 변경 전 1
         max_tokens: 50, // 변경 전 520
-        top_p: 0.8, // 변경 전 1
+        top_p: 0.1, // 변경 전 1
         frequency_penalty: 0,
         presence_penalty: 0,
     });
